2602eda55f76752ed9c79e1eb6697e1e0f702eb3
Raghav Raj Dwivedi <71825024+ronoxdegrand@users.noreply.github.com>
2021-11-10 14:55:34 +0530

    Fixed stuffs

--- oM2M/oM2M.ino
+++ oM2M/oM2M.ino
@@ -1,45 +1,27 @@
 #include "WiFi.h"
 #include "HTTPClient.h"
-#include "ArduinoJson.h"
+#include "Arduino_JSON.h"
 
 char *ssid = "";
 char *pwd = "";
-//String cse_ip = "192.168.43.59";
-String cse_ip = "https://esw-onem2m.iiit.ac.in";
 
-StaticJsonBuffer<200> jsonBuffer;
-JsonObject& user_data = jsonBuffer.createObject();
-JsonObject& temp_user_data = jsonBuffer.createObject();
-JsonObject& sensor_data = jsonBuffer.createObject();
+String server = "https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/";
 
-String server = cse_ip+"/~/in-cse/in-name/";
+const char* ntpServer = "pool.ntp.org";
+const long timeOffset = 19800;
+const int daylightOffset = 0;
 
-String createCI(String server, String ae, String cnt, String val)
+String createCI(String val)
 {
   HTTPClient http;
   http.begin("https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/Team-32/Node-1/Data");
-  Serial.println("https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/Team-32/Node-1/Data");
-//  String postRequest = String() + "POST " + url + " HTTP/1.1\r\n" +
-//                       "Host: " + CSE_IP + ":" + CSE_HTTP_PORT + "\r\n" +
-//                       "X-M2M-Origin: " + CSE_M2M_ORIGIN + "\r\n" +
-//                       "Content-Type: application/json;ty=" + ty + "\r\n" +
-//                       "Content-Length: " + rep.length() + "\r\n"
-//                       "Connection: close\r\n\n" +
-//                       rep;
-//                       
+  Serial.println("https://esw-onem2m.iiit.ac.in/~/in-cse/in-name/Team-32/Node-1/Data");                     
   http.addHeader("X-M2M-Origin", "lV2tfIWL8r:DPiZn9pxhA");
   http.addHeader("Content-Type", "application/json;ty=4");
-  http.addHeader("Content-Length", "100");
-  http.addHeader("Connection", "close");
-//  http.addHeader("Content-Type", "application/json;ty=3");
-//  http.addHeader("Content-Length", "100");
-//  http.addHeader("Connection", "keep-alive");
-//  http.addHeader("Accept-Encoding", "gzip, deflate, br");
-//  http.addHeader("Accept", "*/*");
-  int code = http.POST("{\"m2m:cin\": {\"lbl\": \"hi\", \"con\": \"12\" }}");
+  int code = http.POST("{\"m2m:cin\": {""\"con\":\"" + val + "\"""}}");
  
   http.end();
-  Serial.print("shit: ");
+  Serial.print("code: ");
   Serial.println(code);
   if(code==-1)
   {
@@ -51,9 +33,6 @@ String createCI(String server, String ae, String cnt, String val)
 void setup()
 {
   Serial.begin(115200);
-
-//  WiFi.mode(WIFI_STA);
-//  WiFi.disconnect();
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pwd);
@@ -73,44 +52,36 @@ void setup()
  
   Serial.print(F("Connected. My IP address is: "));
   Serial.println(WiFi.localIP());
+
+  configTime(timeOffset, daylightOffset, ntpServer);
+  
   Serial.println("Setup done");
 }
 
-
-int total_time=0;
 void loop()
 {
-  //To GET values
-  //http.begin("http://192.168.0.113:8080/~/in-cse/in-name/verte001/DATA/la");
-  //http.begin("http://192.168.0.113:8080/~/in-cse");
-  //int httpCode = http.GET();
+  struct tm timeinfo;
+  String timetosend;
+  if (!getLocalTime(&timeinfo))
+    timetosend = "Failed to obtain time";
+  else
+    timetosend = String(timeinfo.tm_year + 1900) + String(timeinfo.tm_mon + 1) + String(timeinfo.tm_mday) + String(timeinfo.tm_hour) + String(timeinfo.tm_min) + String(timeinfo.tm_sec);
 
+  Serial.println(timetosend);
+  
   int ntu = random(100);
+  int otherdata = random(50);
+  int nextdata = random(10);
+  int lmaowhat = random(1000);
+  int lasttime = random(400);
   
   Serial.print ("Sensor Output (ntu):");
   Serial.println (ntu);
-  Serial.println();
 
-  String sensor2 = String(ntu); 
-
-  String sensor_string = sensor2;
-
-  sensor_string = "\"" + sensor_string + "\""; // DO NOT CHANGE THIS LINE
+  String sensor_string = "[" + timetosend + "," + String(ntu) + String(otherdata) + String(nextdata) + String(lmaowhat) + String(lasttime) + "]";
 
   // Send data to OneM2M server
-  createCI(server, "Team_32", "Node_1", sensor_string);
-  
-//  Serial.println(total_time);
-//  float ss = sensorValue;
-//  float s = (ss*5.0)/4096.0;
-//  Serial.println(sensorValue);
+  createCI(sensor_string);
 
-//  delay(500);
-  delay(2000); // DO NOT CHANGE THIS LINE/
-//  total_time++;
-//  if(total_time==10)
-//  {
-//    ESP.restart();
-//  }
-//  
+  delay(2000);
 }
